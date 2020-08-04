79a1fcbd3cc54949a071e8fb6409e9700cb67054
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-08-04 17:34:55 +0800

    [XingfengYang] #N/A

---
+++ labzion.github.io/precache-manifest.07b7ecbbcab5db52d6460c165a9d7b31.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10b6238607e200b5ecd486dd706c80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9dfdd5a830744bdcca",
+    "url": "/static/css/2.3db8585d.chunk.css"
+  },
+  {
+    "revision": "2a445903e206fe91429a",
+    "url": "/static/css/main.47d9d860.chunk.css"
+  },
+  {
+    "revision": "7d9dfdd5a830744bdcca",
+    "url": "/static/js/2.37e80e0c.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/static/js/2.37e80e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a445903e206fe91429a",
+    "url": "/static/js/main.47dd4006.chunk.js"
+  },
+  {
+    "revision": "83cc45e384b93c531a3f",
+    "url": "/static/js/runtime-main.98b6a63a.js"
+  },
+  {
+    "revision": "2f2b33cab65ce1392e78b8b7521a7a0f",
+    "url": "/static/media/Yuxia_Gao.2f2b33ca.jpeg"
+  },
+  {
+    "revision": "d31f61cf80b56d9c7979623302566344",
+    "url": "/static/media/banner1.d31f61cf.webp"
+  },
+  {
+    "revision": "69ca6c297a8759f40ca276d62b0fceb1",
+    "url": "/static/media/duvm.69ca6c29.png"
+  },
+  {
+    "revision": "e04c63acc6e72703797508c0ef72bb00",
+    "url": "/static/media/haoli.e04c63ac.jpg"
+  },
+  {
+    "revision": "74ad96e8700537aeb5aca4aadd8c809f",
+    "url": "/static/media/jingwang.74ad96e8.jpeg"
+  },
+  {
+    "revision": "ce0d8382e190814047bfa9bd78038c0a",
+    "url": "/static/media/laowang.ce0d8382.jpeg"
+  },
+  {
+    "revision": "1042359e8dc9863953db290fecd58543",
+    "url": "/static/media/liangliang.1042359e.jpeg"
+  },
+  {
+    "revision": "2dbb488c859ee8b863f48774f0a38505",
+    "url": "/static/media/liuhubing.2dbb488c.jpg"
+  },
+  {
+    "revision": "80f67a44776791ed9af98b4a35d70adc",
+    "url": "/static/media/liwang.80f67a44.png"
+  },
+  {
+    "revision": "221a593ce75773bd964b499e2068bd44",
+    "url": "/static/media/logo_large.221a593c.png"
+  },
+  {
+    "revision": "6fc40e7a919610776b0c07f9faa9fce7",
+    "url": "/static/media/luke.6fc40e7a.png"
+  },
+  {
+    "revision": "b5e124c1556b056fd871f4362e87db6a",
+    "url": "/static/media/lvju.b5e124c1.jpeg"
+  },
+  {
+    "revision": "5e83a95303762f064bbdc434415ea274",
+    "url": "/static/media/ndfs.5e83a953.png"
+  },
+  {
+    "revision": "83b4bec70708208b68096588075c58da",
+    "url": "/static/media/nengcao.83b4bec7.png"
+  },
+  {
+    "revision": "1c6c6613df801b8d2099b73fb53bc541",
+    "url": "/static/media/shifu.1c6c6613.jpeg"
+  },
+  {
+    "revision": "60f31b9e5fe6f1702b23aec3e055a619",
+    "url": "/static/media/shuage.60f31b9e.jpeg"
+  },
+  {
+    "revision": "5e5683b4cc18f8f489ba3677a4884222",
+    "url": "/static/media/taoge.5e5683b4.png"
+  },
+  {
+    "revision": "bb2631682ecf4e104afa8004627e637f",
+    "url": "/static/media/tinygl.bb263168.png"
+  },
+  {
+    "revision": "61a11956a5617f50ac0f358f2b9a1749",
+    "url": "/static/media/xingfeng.61a11956.png"
+  },
+  {
+    "revision": "0644f911dbb499d68c0e95582dd288f8",
+    "url": "/static/media/xuanliu_shenzhen.0644f911.jpeg"
+  },
+  {
+    "revision": "7e4b1647b43f6238607e4c070f1d3456",
+    "url": "/static/media/yiyang.7e4b1647.jpeg"
+  }
+]);
