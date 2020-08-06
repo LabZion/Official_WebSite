425275c10e134f2ea68753ac8b161aa57ae98302
windcrystal0513 <wuyunfeng1234123@163.com>
2020-08-06 14:19:48 +0800

    add medicine case

--- src/pages/cases/Cases.js
+++ src/pages/cases/Cases.js
@@ -207,7 +207,7 @@ const cases = {
                     '\n' +
                     '我们发现客户的每台服务器都有两根网线，一条接到内网交换机，一条接外网交换机。而且没有对数据做任何保护，服务器之间也没有做隔离，如果一台服务器被攻破，其它的也都会暴露。\n' +
                     '\n' +
-                    '![311](/Users/wuyunfeng/Desktop/311.png)\n' +
+                    '![img](https://raw.githubusercontent.com/LabZion/Official_WebSite/master/public/picture/cases/311.png)\n' +
                     '\n' +
                     '\n' +
                     '\n' +
@@ -219,7 +219,7 @@ const cases = {
                     '\n' +
                     '​        使用VPN防火墙替换原来的防火墙，保障运维安全。原始的window远程桌面存在很多安全漏洞，比如永恒之蓝。使用VPN的方式，用户不能直接访问服务器，需要通过VPN拨号才能访问。\n' +
                     '\n' +
-                    '![312](/Users/wuyunfeng/Desktop/312.png)\n' +
+                    '!![img](https://raw.githubusercontent.com/LabZion/Official_WebSite/master/public/picture/cases/312.png)\n' +
                     '\n' +
                     '### 客户价值\n' +
                     '\n' +
