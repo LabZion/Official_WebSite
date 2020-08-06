2ed95bfcee04d370c2c333e118404ed2950dca13
windcrystal0513 <wuyunfeng1234123@163.com>
2020-08-06 10:30:50 +0800

    modify Splash

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -17,43 +17,43 @@ class Splash extends Component {
                     <Container className="SplashContainer">
                         <Carousel>
                             <Carousel.Item>
-                                <Container className={"bannerContainer2"}>
-                                    <Row>
-                                        <Col md={2}></Col>
-                                        <Col md={8}>
-                                            <h1 className={"banner-title"}>ThoughtWorks Security</h1>
-                                            <br/>
-                                            <h3>让我们将安全品质注入您的商业世界，让您的资产固若金汤。</h3>
-                                            {/*<h3>Let's drive security into the fabric of your business together</h3>*/}
-                                        </Col>
-                                    </Row>
-                                </Container>
-
-                                {/*<Carousel.Caption>*/}
-                                {/*    <h3>ThoughtWorks Security</h3>*/}
-                                {/*    <p>Let's drive security into the fabric of your business together</p>*/}
-                                {/*</Carousel.Caption>*/}
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Container className={"bannerContainer2"}>
-                                    <Row>
-                                        <Col md={2}></Col>
-                                        <Col md={8}>
-                                            <h1 className={"banner-title"}>ThoughtWorks Security</h1>
-                                            <br/>
-                                            <h3>让我们将安全品质注入您的商业世界，让您的资产固若金汤。</h3>
-                                            {/*<h3>Let's drive security into the fabric of your business together</h3>*/}
-                                        </Col>
-                                    </Row>
-                                </Container>
-
-                                {/*<Carousel.Caption>*/}
-                                {/*    <h3>ThoughtWorks Security</h3>*/}
-                                {/*    <p>Let's drive security into the fabric of your business together</p>*/}
-                                {/*</Carousel.Caption>*/}
-                            </Carousel.Item>
-                            <Carousel.Item>
                                 <Container className={"bannerContainer3"}>
+                                    <Row>
+                                        <Col md={2}></Col>
+                                        <Col md={8}>
+                                            <h1 className={"banner-title"}>ThoughtWorks Security</h1>
+                                            <br/>
+                                            <h3>让我们将安全品质注入您的商业世界，让您的资产固若金汤。</h3>
+                                            {/*<h3>Let's drive security into the fabric of your business together</h3>*/}
+                                        </Col>
+                                    </Row>
+                                </Container>
+
+                                {/*<Carousel.Caption>*/}
+                                {/*    <h3>ThoughtWorks Security</h3>*/}
+                                {/*    <p>Let's drive security into the fabric of your business together</p>*/}
+                                {/*</Carousel.Caption>*/}
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Container className={"bannerContainer2"}>
+                                    <Row>
+                                        <Col md={2}></Col>
+                                        <Col md={8}>
+                                            <h1 className={"banner-title"}>ThoughtWorks Security</h1>
+                                            <br/>
+                                            <h3>让我们将安全品质注入您的商业世界，让您的资产固若金汤。</h3>
+                                            {/*<h3>Let's drive security into the fabric of your business together</h3>*/}
+                                        </Col>
+                                    </Row>
+                                </Container>
+
+                                {/*<Carousel.Caption>*/}
+                                {/*    <h3>ThoughtWorks Security</h3>*/}
+                                {/*    <p>Let's drive security into the fabric of your business together</p>*/}
+                                {/*</Carousel.Caption>*/}
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Container className={"bannerContainer1"}>
                                     <Row>
                                         <Col md={2}></Col>
                                         <Col md={8}>
