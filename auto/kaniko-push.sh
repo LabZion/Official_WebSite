e917a112e44a1e22f1ed2c10632d34abbabf4873
Hao Li <hayatelee@gmail.com>
2020-11-11 03:01:03 +0800

    [hao] try to fix ecr permission issue

--- auto/kaniko-push.sh
+++ auto/kaniko-push.sh
@@ -4,5 +4,10 @@ cd $(dirname $0)/..
 
 . ./auto/set-env-vars
 
+mkdir -p ~/.docker
+cat > ~/.docker/config.json <<EOF
+    { "credsStore": "ecr-login" }
+EOF
+
 /kaniko/crane push image.tar ${DOCKER_IMAGE}:${TAG}
 /kaniko/crane push image.tar ${DOCKER_IMAGE}:latest
